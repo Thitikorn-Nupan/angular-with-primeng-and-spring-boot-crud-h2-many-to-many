5334dbb08c249e860ad834e3754c907f4f32e2b7
Thitikorn Nupan <thitikorn.n@g-able.com>
2025-06-25 12:13:33 +0700

    commit

---
+++ src/main/java/com/ttknp/springbootcrudh2manytomany/SpringBootCrudH2ManyToManyForAngularWithPrimengApplication.java
@@ -0,0 +1,13 @@
+package com.ttknp.springbootcrudh2manytomany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudH2ManyToManyForAngularWithPrimengApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCrudH2ManyToManyForAngularWithPrimengApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ttknp/springbootcrudh2manytomany/SpringBootCrudH2ManyToManyForAngularWithPrimengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttknp.springbootcrudh2manytomany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudH2ManyToManyForAngularWithPrimengApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
