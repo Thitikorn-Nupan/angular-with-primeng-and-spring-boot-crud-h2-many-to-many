b97e3cfc6e6296b85125a066fc9c51dca874a971
thitikorn_nupan@outlook.co.th <thitikorn_nupan@outlook.co.th>
2025-07-08 23:17:40 +0700

    commit
    - fix bug on dynamic update statement on jdbc helper
    
    # Conflicts:
    #	src/main/java/com/ttknp/springbootcrudh2manytomany/controllers/ActorController.java

--- src/main/java/com/ttknp/springbootcrudh2manytomany/controllers/ActorController.java
+++ src/main/java/com/ttknp/springbootcrudh2manytomany/controllers/ActorController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:4200","http://thitkorn-nupan.com"})
 @Slf4j
 @CommonRestAPI(configPath = {"/actor","/actors"})
 public class ActorController {
